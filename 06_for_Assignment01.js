f49bdef0c5d29904dc032afb92979e7d735b309c
GavhanePriya <priyashitole064@gmail.com>
2023-08-01 14:05:32 +0530

    using the for loop and while loop
    using the for loop and while loop

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,38 @@
+console.log(`---------------------------------Assignment 1---------------------------`);
+console.log(`Print a a Number 5 to 15`);
+for (let index = 5; index < 15; index++) {
+    console.log(index);
+}
+console.log(`---------------------------------`);
+console.log(`Print a Number 50 to 40 `);
+for (let index = 50; index > 40; index--) {
+    console.log(index); 
+}
+console.log(`Print a First 15 odd numbers`);
+console.log(`-----------------------------`);
+for (let index = 1; index <30; index=index+2) {
+    console.log(index);
+    
+}
+console.log(`------------------------------`);
+console.log(`Print a First 10 even number`);
+for (let index = 0; index <20; index=index+2) {
+    console.log(index);
+
+}
+console.log(`-------------------------------`);
+console.log(`Print the table of 5`);
+for (let index = 5; index <=50; index=index+5) {
+    console.log(index);   
+}
+console.log(`-------------------------------`);
+console.log(`Print the table of 10 `);
+for (let index = 10; index <=100; index=index+10) {
+    console.log(index);
+}
+console.log(`-------------------------------`);
+console.log(`Print the 100 to 10`);
+for (let index = 100; index >=10 ; index=index-10) {
+    console.log(index);
+}
+console.log(`------------------------End the Assignment------------------`);
--- 06_while_Assignment01.js
+++ 06_while_Assignment01.js
@@ -1,6 +1,54 @@
 console.log(`-------------------------------Assignmnet 1--------------------`);
-let i=5;
+console.log(`Print the number 5 to 15`);
+var i=5;
 while (i<=15) {
     console.log(i);
     i++
+    console.log(`--------------------continive---------------------------------`);
 }
+console.log(`----------------------`);
+console.log(`print the number 50 to 40 `);
+var index = 50;
+while (index>=40) {
+    console.log(index);
+    index--
+}
+console.log(`------------------------`);
+console.log(`Print the First 15 odd number`);
+var index = 1;
+while (index<=30) {
+    console.log(index);
+    index=index+2;
+    
+}
+console.log(`----------------------`);
+console.log(`Print the Frist 10 even number`);
+var index = 0;
+while (index<=20) {
+    console.log(index);
+    index = index+2;
+    
+}
+console.log(`-------------------------`);
+console.log(`Print the table of 5th`);
+var index = 5;
+while (index<=50) {
+    console.log(index);
+    index = index+5   
+}
+console.log(`----------------------------`);
+console.log(`Print the table of 10`);
+var index = 10;
+while (index<=100) {
+    console.log(index);
+    index=index+10;
+    
+}
+console.log(`---------------------------`);
+console.log(`Print the reverse table of 10`);
+var index = 100;
+while (index>=10) {
+    console.log(index);
+    index=index-10;
+}
+console.log(`-------------------End the Assignment------------`);

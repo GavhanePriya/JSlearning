09342f3b0f7626cc1e322e2b6d34ac8d49029ec1
GavhanePriya <priyashitole064@gmail.com>
2023-08-21 22:25:29 -0700

    all changes are commit and push
    all changes are commit and push

--- restParameterAssign.js
+++ restParameterAssign.js
@@ -2,11 +2,11 @@ function add(num1, num2, ...num ){
     
        var sum = 0;
        for (let index = 0; index < num.length; index++) {
-       sum+=num[index];    
+                sum+=num[index];    
        }
        console.log(`num1 = ${num1},  num2 = ${num2}`);
        console.log(num);
-       let result = num1+num2+sum;
+       let result =+sum;
        console.log(`The total  Addition of Array: ${result} `);
    }
    

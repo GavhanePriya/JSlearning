2249504cf4a1d3e4b267f223a52ef38fa6467877
GavhanePriya <priyashitole064@gmail.com>
2023-08-23 00:18:57 -0700

    Using forEach loop
    Using forEach loop

---
+++ 23_forEachAssignA.js
@@ -0,0 +1,42 @@
+console.log(`-----------------------------Assignment1---------------------------`);
+console.log(`------------------------------step1----------------------`);
+const arrayNumber = [1 , -7 , 40 , 502 , -77 , 91 , 0 , 108 , 89 , -601];
+arrayNumber.forEach((currentValue , index , arrayNumber) => {
+    console.log(currentValue,index,arrayNumber );
+});
+console.log(`---------------------------step2---------------------------`);
+arrayNumber.forEach((currentValue) => {
+    if (currentValue>0) {
+        console.log(`Positive number is  : ${currentValue}`);
+    }
+});
+console.log(`---------------------------step3-------------------------`);
+const negativeNumber = []
+arrayNumber.forEach((currentValue) => {
+    if (currentValue < 0) {
+        negativeNumber.push(currentValue)
+        console.log(`Negative number in array is : ${negativeNumber}`);
+    }
+});
+console.log(`--------------------------------step4--------------------`);
+arrayNumber.forEach((currentValue) => {
+    if (currentValue%2==0) {
+        console.log(`The Even number in array is : ${currentValue}`);  
+    }
+});
+console.log(`------------------------------step5---------------------`);
+let sum=0;
+arrayNumber.forEach((currentValue) => {
+       sum+=currentValue 
+    }
+    );
+    console.log(`sum of all elment in Array is : ${sum}`);
+    console.log(`----------------------------step6-----------------`);
+    arrayNumber.forEach((currentValue,index) => {
+        if (index %2== 0) {
+            console.log(`the even index in array is :${index} is ${currentValue}`);
+            
+        }
+        
+    });
+    console.log(`----------------------------------------------------------`);
